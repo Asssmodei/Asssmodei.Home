e3eb317769c3b6a5308b310bae9ef4608e3e0fa5
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2021-09-25 21:37:54 +0300

    Delete Homework 2.3.py

--- homework 2/Homework 2.3.py
+++
@@ -1,9 +0,0 @@
-memory = []
-for i in range(1, 5):
-    memory[i] = int(input())
-    if memory[i] < 10:
-        continue
-    else:
-        break
-        print('Введено число больше 10')
-    print('\n', memory[i])

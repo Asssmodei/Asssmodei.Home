428192327a9a23dfb5db8ccf88f0ac9471c834eb
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2021-10-13 13:34:20 +0300

    Update Homework №2.6.py

--- homework 2/Homework №2.6.py
+++ homework 2/Homework №2.6.py
@@ -1,9 +1,9 @@
 print('Введите строку')
 a = list(input())
 x = []
-for i in range(0, len(a)):
-    if a[i].isdigit():
-        x.append(a[i])
+for i in a:
+    if i.isdigit():
+        x.append(i)
 print('Какой элемент ищем?')
 k = int(input())
 print(x[k-1])  

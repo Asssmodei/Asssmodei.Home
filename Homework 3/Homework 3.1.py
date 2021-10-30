26de646bcd5177db80b2b57b495c7ec8fbc10e8f
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2021-10-30 07:58:30 +0300

    Add files via upload

---
+++ Homework 3/Homework 3.1.py
@@ -0,0 +1,12 @@
+def print_string():
+    list1 = []
+    while True:
+        x = input()
+        if x == "":
+            break
+    else:
+        list1.append(x)
+    s = list1[0]
+    for i in range(1, len(list1)-1):
+        s += " " + list1[i]
+    print(s)

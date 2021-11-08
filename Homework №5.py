2ae045605c7ac6ada93dde3df0700a10b82fb357
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2021-11-08 21:40:15 +0300

    Add files via upload
    Бинарный поиск

---
+++ Homework №5.py
@@ -0,0 +1,23 @@
+def binarius():
+    x = int(input())  # загаданное число
+    a, b = int(input())  # границы
+    f = True
+    while f:
+        print('Число больше или меньше чем', (a + b) / 2, '?')
+        d = input()
+        print('Число больше или меньше на 1?')
+        q = input()
+        if d == 'больше':
+            a = (a + b) / 2
+            f = True
+        else:
+            b = (a + b) / 2
+            f = True
+        if q == 'нет':
+            continue
+        elif q == 'Больше':
+            z = (a + b) / 2 + 1
+        else:
+            z = (a + b) / 2 - 1
+    if z == x:
+        print('УРА')

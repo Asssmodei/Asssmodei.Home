6e7cffe19f86422f7dc3498576b443f5b3435f9b
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2021-11-11 08:24:28 +0300

    Add files via upload
    дочинил

--- Homework №5.py
+++ Homework №5.py
@@ -1,22 +1,27 @@
 def binarius(x, memory):
     memory = sorted(memory)
     f = True
-    if x < memory[0] or x > memory[-1]:
+    if x == memory[0]:
+        return memory[0]
+    elif x == memory[-1]:
+        return memory[-1]
+    elif x < memory[0] or x > memory[-1]:
         return None
     while f:
-        z = ((memory[0] + memory[-1]) / 2) - x
-        print('Число больше или меньше чем', z, '?')
+        print('Число больше или меньше чем', int(((memory[0] + memory[-1]) / 2)), '?')
         d = input()    # я спрашиваю у пользователя потому что программа изначально не знает число и типо угадывает
-        if d == 'больше':
-            memory[0] = (memory[0] + memory[-1]) / 2
+        if x == int((memory[0] + memory[-1]) / 2):
+            return int((memory[0] + memory[-1]) / 2)
+        elif d == 'больше':
+            memory[0] = int((memory[0] + memory[-1]) / 2)
             f = True
         else:
-            memory[-1] = (memory[0] + memory[-1]) / 2
+            memory[-1] = int((memory[0] + memory[-1]) / 2)
             f = True
+        z = int(((memory[0] + memory[-1]) / 2)) - x
         if z != 1 or z != -1:
             continue
         elif z == 1:
-            return ((memory[0] + memory[-1]) / 2) - 1
+            return int(((memory[0] + memory[-1]) / 2)) - 1
         else:
-            return (memory[0] + memory[-1]) / 2 + 1
-
+            return int(((memory[0] + memory[-1]) / 2)) + 1

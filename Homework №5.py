842870bdd728c9a48ebadd50768440eaa6b6792f
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2021-11-26 10:56:54 +0300

    Add files via upload
    Прикрутил тесты

--- Homework №5.py
+++ Homework №5.py
@@ -19,5 +19,18 @@ def binarius(x, memory):
             print(memory[-1])
             f = True
         z = ((memory[0] + memory[-1]) / 2) - x
-        if -0.6 < z < 0.6:
+        if -0.51 < z < 0.51:
+            print(round((memory[0] + memory[-1]) / 2))
             return round((memory[0] + memory[-1]) / 2)
+
+
+assert binarius(1, [1, 2, 3, 5, 4, 6, 7, 8, 9, 10]) == 1  # выполнено
+assert binarius(5, [1, 2, 3, 5, 4, 6, 7, 8, 9, 10]) == 5 # выполнено
+assert binarius(7, [1, 2, 3, 5, 4, 6, 7, 8, 9, 10]) == 7  # выполнено
+k = []
+for i in range(1, 101):    # мне просто лень список на 100 писать
+    k.append(i)
+assert binarius(42, k) == 42  # выполенно
+assert binarius(99, k) == 99  # выполнено
+assert binarius(99, k) == 100  # не выполнено
+assert binarius(67, k) == 66  # не выполнено

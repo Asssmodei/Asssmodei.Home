c2f35bd145c23fe46dcc0ef4296f23c7824035d3
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2022-02-19 08:57:08 +0300

    Update Homework №2.0.1.py

--- 2.0.1/Homework №2.0.1.py
+++ 2.0.1/Homework №2.0.1.py
@@ -1,17 +1,15 @@
 class Fraction:
-    ch:int
-    zn:int
-    
-    def __init__(self, zn=1, ch=0):
+
+    def __init__(self, zn: int = 1, ch: int = 0) -> None:
         self.zn = zn
         self.ch = ch
 
-    def inner(self):
+    def inner(self) -> None:
         self.ch = int(input('Введите числитель '))
         self.zn = int(input('Введите знаменатель '))
         if self.zn == 0:
             raise ZeroDivisionError
 
-    def __str__(self):
+    def __str__(self) -> str:
         return  f"{self.ch}/{self.zn}"
     

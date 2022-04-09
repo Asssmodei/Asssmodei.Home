863c4c9284c6eb655ee9dea3a0172805361fc4bd
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2022-04-09 09:23:25 +0300

    Update test_irreduceablefraction.py

--- tests/test_irreduceablefraction.py
+++ tests/test_irreduceablefraction.py
@@ -1,6 +1,14 @@
-from app.main import Fraction, IrreduceableFraction
+from app.main import IrreduceableFraction
 
 
 def test_irreduceable():
     q = IrreduceableFraction(2, 4)
-    assert q == Fraction(1, 2)
+    assert q == IrreduceableFraction(1, 2)
+
+
+def test_inner(mocker):
+    mocker.patch('builtins.input', side_effect=['5', '10'])
+    f = IrreduceableFraction()
+    f.inner()
+    assert f == IrreduceableFraction(1, 2)
+    

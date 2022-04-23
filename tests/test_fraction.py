6da259eac31b20a76223aa6a120e1c81a0fcfd11
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2022-04-23 09:37:22 +0300

    Update test_fraction.py

--- tests/test_fraction.py
+++ tests/test_fraction.py
@@ -1,27 +1,27 @@
-from app.main import Fraction
+def test_add(a, b, c):
+    assert a + b == c
 
 
-def test_eq():
-    assert Fraction(2, 1) == Fraction(2, 1)
+@pytest.mark.parametrize(
+    ('a', 'b', 'c'),
+    [[Fraction(-2, 5), Fraction(3, 5), Fraction(-5, 5)],
+     [Fraction(11, -6), Fraction(2, 3), Fraction(-15, 6)],
+     [Fraction(2, -2), Fraction(1, -2), Fraction(-1, 2)]
+     ]
+)
+def test_sub(a, b, c):
+    print(a - b)
+    assert a - b == c
 
 
-def test_not_eq():
-    assert Fraction(3, 2) != Fraction(2, 2)
+
+def test_mul():
+    a = Fraction(3, 4)
+    b = Fraction(2, 5)
+    assert a * b == Fraction(6, 20)
 
 
-def test_reduce():
-    q = Fraction(2, 4)
-    q.reduce()
-    assert q == Fraction(1, 2)
-
-
-def test_inner(mocker):
-    mocker.patch('builtins.input', side_effect=['1', '2'])
-    f = Fraction()
-    f.inner()
-    assert f == Fraction(1, 2)
-
-
-def test_str():
-    q = Fraction(1, 2)
-    assert str(q) == '1/2'
+def test_truediv():
+    a = Fraction(3, 4)
+    b = Fraction(2, 2)
+    assert a / b == Fraction(6, 8)

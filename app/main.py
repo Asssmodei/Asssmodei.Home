1953e2446c6c789e4f2ba479fd85c425afee1921
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2022-04-23 09:38:07 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import math
+from math import *
+
+
 class Fraction:
 
     def __init__(self, ch: int = 0, zn: int = 1) -> None:
@@ -15,13 +19,39 @@ class Fraction:
 
     def __eq__(self, other: "Fraction") -> bool:
         return self.ch == other.ch and self.zn == other.zn
+    
+    def __add__(self, other):
+        k = self.__lcm(self.zn, other.zn)
+        d = self.ch * (k // self.zn) + other.ch * (k // other.zn)
+        return Fraction(d, k)
+
+    def __sub__(self, other):
+        k = math.lcm(self.zn, other.zn)
+        d = self.ch * (k // self.zn) - other.ch * (k // other.zn)
+        return Fraction(d, k)
+
+    def __mul__(self, other):
+        k = self.ch * other.ch
+        d = self.zn * other.zn
+        return Fraction(k, d)
+
+    def __truediv__(self, other):
+        k = self.ch * other.zn
+        d = self.zn * other.ch
+        return Fraction(k, d)
 
     def reduce(self) -> None:
         if self.ch == 0:
             return
-        k = gcd(self.zn, self.ch)
+        k = self.__gcd(self.zn, self.ch)
         self.zn = self.zn // k
         self.ch = self.ch // k
+
+    def __lcm(self, a, b):
+        return math.lcm(a, b)
+
+    def __gcd(self, a, b):
+        return math.gcd(a, b)
 
 
 class IrreduceableFraction(Fraction):

ba5bed0066e67f3c934005a9378846b76d8bc917
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2022-04-23 09:37:42 +0300

    Update test_irreduceablefraction.py

--- tests/test_irreduceablefraction.py
+++ tests/test_irreduceablefraction.py
@@ -11,4 +11,27 @@ def test_inner(mocker):
     f = IrreduceableFraction()
     f.inner()
     assert f == IrreduceableFraction(1, 2)
-    
+
+
+def test_add():
+    a = IrreduceableFraction(2, 4)
+    b = IrreduceableFraction(4, 6)
+    assert a + b == IrreduceableFraction(7, 6)
+
+
+def test_sub():
+    a = IrreduceableFraction(2, 2)
+    b = IrreduceableFraction(1, 2)
+    assert a - b == IrreduceableFraction(1, 2)
+
+
+def test_mul():
+    a = IrreduceableFraction(2, 4)
+    b = IrreduceableFraction(1, 5)
+    assert a * b == IrreduceableFraction(1, 10)
+
+
+def test_truediv():
+    a = IrreduceableFraction(4, 2)
+    b = IrreduceableFraction(5, 3)
+    assert a / b == IrreduceableFraction(6, 10)

df56480dc0271d48017bc6f7f7ee8e713b6c8c6f
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2022-05-14 09:36:13 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,46 +11,51 @@ class Fraction:
     def inner(self) -> None:
         self.ch = int(input('Введите числитель '))
         self.zn = int(input('Введите знаменатель '))
-        if self.zn == 0:
+        if not self.zn:
             raise ZeroDivisionError
 
     def __str__(self) -> str:
         return f"{self.ch}/{self.zn}"
 
+    def __repr__(self):
+        return str(self)
+
     def __eq__(self, other: "Fraction") -> bool:
         return self.ch == other.ch and self.zn == other.zn
     
-    def __add__(self, other):
+    def __add__(self, other: 'Fraction') -> 'Fraction':
         k = self.__lcm(self.zn, other.zn)
         d = self.ch * (k // self.zn) + other.ch * (k // other.zn)
-        return Fraction(d, k)
+        return self.__class__(d, k)
 
-    def __sub__(self, other):
-        k = math.lcm(self.zn, other.zn)
+    def __sub__(self, other: 'Fraction') -> 'Fraction':
+        k = self.__lcm(self.zn, other.zn)
         d = self.ch * (k // self.zn) - other.ch * (k // other.zn)
-        return Fraction(d, k)
+        return self.__class__(d, k)
 
-    def __mul__(self, other):
+    def __mul__(self, other: 'Fraction') -> 'Fraction':
         k = self.ch * other.ch
         d = self.zn * other.zn
-        return Fraction(k, d)
+        return self.__class__(k, d)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: 'Fraction') -> 'Fraction':
         k = self.ch * other.zn
         d = self.zn * other.ch
-        return Fraction(k, d)
+        return self.__class__(k, d)
 
     def reduce(self) -> None:
-        if self.ch == 0:
-            return
         k = self.__gcd(self.zn, self.ch)
+        # Знаменатель всегда должен быть положительным
+        if self.zn < 0:
+            self.ch = -self.ch
+            self.zn = -self.zn
         self.zn = self.zn // k
         self.ch = self.ch // k
 
-    def __lcm(self, a, b):
+    def __lcm(self, a: int, b: int) -> int:
         return math.lcm(a, b)
 
-    def __gcd(self, a, b):
+    def __gcd(self, a: int, b: int) -> int:
         return math.gcd(a, b)
 
 

6d6bbb879cceae117ee65f1ab99e889771f84fa9
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2022-07-03 19:54:29 +0300

    Add files via upload

--- Filth/Filth/urls.py
+++ Filth/Filth/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('News.urls')),
+    path('admin/', admin.site.urls)
 
 ]
 
--- Filth/News/views.py
+++ Filth/News/views.py
@@ -5,7 +5,7 @@ from .models import News
 
 def index(request):
     print(request)
-    news = News.objects.order_by('-created_at')
+    news = News.objects.all()
     context = {'news': news, 'title': 'Cписок новостей'}
     return render(request, 'news/index.html', context)
 # Create your views here.
